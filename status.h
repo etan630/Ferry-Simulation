4dfb715cbb14975479258fb4342768c5cda3d71a
etan630 <79673596+etan630@users.noreply.github.com>
2023-09-06 23:02:05 -0400

    Create status.h

---
+++ status.h
@@ -0,0 +1,13 @@
+#ifndef STATUS_H
+#define STATUS_H
+
+enum status { FAILURE, SUCCESS }; //failure is 0, success is 1
+typedef enum status Status;
+
+enum boolean { FALSE, TRUE };
+typedef enum boolean Boolean;
+
+enum bank { LEFT, RIGHT };
+typedef enum bank Bank;
+
+#endif
